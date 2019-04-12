ba4fec8a22ea36f362d63c1bb3a162d488f16bd6
Michael Power <powerm3@tcd.ie>
2019-04-12 23:11:06 +0100

    Update quote_of_the_day.py

--- quote_of_the_day.py
+++ quote_of_the_day.py
@@ -5,7 +5,7 @@ import time
 from bs4 import BeautifulSoup
 
 # Set the URL you want to webscrape from
-url = 'https://www.brainyquote.com/quote_of_the_dayl'
+url = 'https://www.brainyquote.com/quote_of_the_day'
 
 # Connect to the URL
 response = requests.get(url)
@@ -17,21 +17,27 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 #quote is in meta tag
 quote = soup.findAll('meta')
+quote_of_the_day = ""
 
-#print(quote)
 
 #find the twtter description as the quote lies in that reference
 for i in quote:
     try:
         if(i.attrs['name']=='twitter:description'):
-            #print(i.attrs['content'])
+        #print(i.attrs['content'])
             quote_of_the_day = i.attrs['content']
     except:
-        a = 1
+        a = "Not everything that is faced can be changed, but nothing can be changed until it is faced.-James Baldwin"
+
+
+#check if quote of the day string is empty
+if not quote_of_the_day:
+    quote_of_the_day = a
 
 #remove quotes
 newstr = quote_of_the_day.replace('"', "")
 
+print(newstr)
 #split string into quote and author
-spit = newstr.split('-')
-print(spit)
+spit = newstr.rsplit('-',1)
+#print(spit)
